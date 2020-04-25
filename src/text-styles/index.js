3d8579758cbe0cffd91116a7255e36d9070d7e6a
lucastobrazil <lucas.arundell@gmail.com>
2020-04-25 21:53:41 +0200

    refactor and add comments

--- src/text-styles/index.js
+++ src/text-styles/index.js
@@ -3,25 +3,25 @@ import { FontSizes, FontWeights, Fonts, ThemeColors } from '@adapt-design-system
 import { getPage, addSupportText, getTextVerticalOffset, createPageWithName, toTitleCase } from '../utils';
 import CONFIG from '../config';
 import { createTextLayer, textAlignments } from './text';
-import { Artboard } from 'sketch/dom';
 
 const document = sketch.getSelectedDocument();
-const validColors = ['text', 'white', 'subduedText', 'primary', 'accent', 'success', 'warning', 'error'];
 
-const textColors2 = Object.keys(ThemeColors)
-    .filter((key) => validColors.includes(key))
-    .reduce((obj, key) => {
-        obj[key] = ThemeColors[key];
-        return obj;
-    }, {});
+/*
+    Colors from the ThemeColors tokens that should be rendered in Sketch
+*/
+const colorWhitelist = ['text', 'white', 'subduedText', 'primary', 'accent', 'success', 'warning', 'error'];
 
+/* 
+    Font Families from the Fonts tokens that should be rendered in Sketch
+    @todo read this from the tokens (need to update tokens package)
 
-// @todo read this from the tokens
+*/
 const fontFamilies = [
     { name: 'default', value: 'Graphik' },
     { name: 'alt', value: 'Tiempos Text' },
 ];
 
+/* Utils */
 const getValues = (data) => {
     const output = {};
     data.map(({ name, value }) => (output[name] = `${value}`));
@@ -34,31 +34,39 @@ const getFontSizes = (data) => {
     return output;
 };
 
+/* Transform Tokens into shape we need */
 const fontSizes = getFontSizes(FontSizes);
 const fontWeights = getValues(FontWeights);
+const fontColors = Object.keys(ThemeColors)
+    .filter((key) => colorWhitelist.includes(key))
+    .reduce((obj, key) => {
+        obj[key] = ThemeColors[key];
+        return obj;
+    }, {});
 
+/* The Main Function */
 const generateTextLayers = () => {
     let yOffset = 0;
     let xOffset = 0;
     let prevAlignmentIndex = 0;
     let prevFontFamilyIndex = 0;
-
     const X_SPACING = 500;
     const Y_SPACING = 130;
     const FONT_FAMILY_SPACING = 5000;
-
     const HELPER_TEXT_COLOR = '#fe7e51';
 
     const _getAdditionalYOffset = (prevFontFamilyIndex, fontFamilyIndex) => {
         /* 
             Check if we're still rendering the same font family, 
-            thus no additional offset.
+            thus no additional Y offset.
         */
         if (prevFontFamilyIndex === fontFamilyIndex) {
             return 0;
         }
         // Update the index as we know it's a new font family
         prevFontFamilyIndex = fontFamilyIndex;
+
+        // If it is a new font family, add an extra Y offset to place them further down the canvas
         return FONT_FAMILY_SPACING;
     };
 
@@ -70,7 +78,7 @@ const generateTextLayers = () => {
             x: xOffset === 0 ? xOffset : xOffset + X_SPACING,
             y: -Y_SPACING * 3,
         }),
-        Object.entries(textColors2).map(([colorName, colorValue], textColorsIndex) => [
+        Object.entries(fontColors).map(([colorName, colorValue], textColorsIndex) => [
             createTextLayer({
                 fontSize: 180,
                 fontWeight: 4,
@@ -112,17 +120,22 @@ const generateTextLayers = () => {
             ),
         ]),
     ]);
+    // Sketch blows up if you have a nested array of layers, so flatten.
     return output.flat(Infinity);
 };
 
 const start = () => {
+    // If no Sketch Page exists, create it.
     let TEXT_PAGE = getPage(document, CONFIG.TEXT_PAGE_NAME);
     if (!TEXT_PAGE) {
         createPageWithName(document, CONFIG.TEXT_PAGE_NAME);
         TEXT_PAGE = getPage(document, CONFIG.TEXT_PAGE_NAME);
     }
+
     // Erase all other layers on the page.
     TEXT_PAGE.layers = [];
+
+    // Generate Layers and add to page.
     TEXT_PAGE.layers.push(
         ...generateTextLayers(),
         addSupportText('These text layers and styles have been automatically generated.', getTextVerticalOffset(-4))
@@ -131,5 +144,6 @@ const start = () => {
 };
 
 export default () => {
+    // Todo add UI 
     start();
 };
